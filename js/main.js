ccf2745e6f70285096387d4cee4790727fd8d550
olejo-hypero <programpsc@gmail.com>
2019-06-04 19:15:09 +0500

    d
    d

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.works__slider').slick({
+          autoplay: false,
+          autoplaySpeed: 2000,
+          arrows: true,
+          slidesToShow: 4,
+    });
+
+    $('.reviews__slider').slick({
+          autoplay: false,
+          autoplaySpeed: 2000,
+          arrows: true,
+          slidesToShow: 2,
+    });
+});
